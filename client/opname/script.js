dbc744015bf86cfc1fda637e2f3eb7c52edf7b68
Dimas Abidzar Fadly <dimasfadly01@gmail.com>
2026-01-15 11:47:59 +0700

    update

--- client/opname/script.js
+++ client/opname/script.js
@@ -206,7 +206,7 @@ const Render = {
         container.innerHTML = `
             <div class="login-wrapper">
                 <div class="login-card">
-                    <a href="https://sparta-alfamart.vercel.app/dashboard/pic/index.html" class="btn-back-link">
+                    <a href="https://sparta-alfamart.vercel.app/dashboard/index.html" class="btn-back-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <line x1="19" y1="12" x2="5" y2="12"></line>
                             <polyline points="12 19 5 12 12 5"></polyline>
